2af612bc29ef32431aeca757d69d2ff490b70e03
dudebh <dudebahrulhayat@gmail.com>
2021-06-25 09:02:55 +0700

    instantiate database

---
+++ migrations/20210625015855-create-appointment.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      CustomerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Customers',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      dropperName: {
+        type: Sequelize.STRING
+      },
+      pickuperName: {
+        type: Sequelize.STRING
+      },
+      appointmentDate: {
+        type: Sequelize.DATEONLY
+      },
+      pickupTime: {
+        type: Sequelize.TIME
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Appointments');
+  }
+};
